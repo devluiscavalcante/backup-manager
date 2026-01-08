cfa0af23e7147dff216c6217fa5b21f010d23836
luis <cleanmec@gmail.com>
2026-01-08 16:59:18 -0300

    feat(auth): implement complete JWT authentication module including controller, service, token utils, security config and DTOs

---
+++ src/main/java/com/backup_manager/application/dto/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.backup_manager.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/backup_manager/application/dto/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.backup_manager.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+
+}
