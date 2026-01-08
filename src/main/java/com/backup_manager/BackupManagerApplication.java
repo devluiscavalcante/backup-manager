b607f7924d2e2f2bbf5ca7721e610a4bf03477ab
luis <cleanmec@gmail.com>
2026-01-08 15:19:48 -0300

    initial commit

---
+++ src/main/java/com/backup_manager/BackupManagerApplication.java
@@ -0,0 +1,13 @@
+package com.backup_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupManagerApplication.class, args);
+	}
+
+}
