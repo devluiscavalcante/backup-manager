f8c79102694964a731c8192ce210b163114c36fc
luis <cleanmec@gmail.com>
2026-01-12 15:13:12 -0300

    refactor(security): clean up JwtAuthFilter imports and exception handling

--- src/main/java/com/backup_manager/infrastructure/security/JwtAuthFilter.java
+++ src/main/java/com/backup_manager/infrastructure/security/JwtAuthFilter.java
@@ -1,11 +1,9 @@
 package com.backup_manager.infrastructure.security;
 
-import io.jsonwebtoken.io.IOException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +12,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
@@ -27,8 +26,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain) throws ServletException, IOException, java.io.IOException {
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
 
@@ -54,11 +53,12 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(auth);
             } else {
+
                 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                 return;
             }
         } catch (Exception e) {
-            // 🔹 Qualquer exceção de parsing → também 401
+
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
